73290b083f018cd88b6d30b334b6c39796d2bf36
cdcya <1094392181@qq.com>
2021-09-07 11:14:35 +0800

    20210907 by cdcya

--- getCite.py
+++ getCite.py
@@ -4,8 +4,8 @@ filename = 'C:/Users/Administrator/Desktop/1231.txt'
 
 urls = gcutil.createUrls(filename)
 # print(urls)
-gcutil.getBibTexSave(urls)
-gcutil.getCiteTextSave(urls)
-gcutil.getCiteTextSave(urls, 1)
+# gcutil.getBibTexSave(urls)
+# gcutil.getCiteTextSave(urls)
+gcutil.getCiteTextSave(urls, 1)     # 0: all cite info., 1: only GB/T 7714 cite info.
 
 print("Finished!")
